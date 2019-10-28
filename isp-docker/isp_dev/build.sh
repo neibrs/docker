a0113fff9975d5d8b1fe2bb496350b8acc45a4b9
John <lijd@eabax.com>
2019-10-28 23:08:45 +0800

    fix.

--- isp-docker/isp_dev/build.sh
+++ isp-docker/isp_dev/build.sh
@@ -3,7 +3,7 @@
 docker pull neibrs/isp_dev
 
 docker rm -f isp_dev
-docker run -d --name isp_dev 114.215.42.138:5000/isp_dev
+docker run -d --name isp_dev neibrs/isp_dev
 if [ -d ./web.old ]; then
   sudo rm -rf ./web.old
 fi
